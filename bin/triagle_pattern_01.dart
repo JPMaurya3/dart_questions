683a0b6165e5b7e55ff88d22339d648706194b6d
SQY USER <jwala.maurya@squareyards.co.in>
2024-10-10 17:35:54 +0530

    alphabet triangle question

---
+++ bin/triagle_pattern_01.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  int i = 1;
+  while (i <= 3) {
+    int j = 1;
+    while (j <= i) {
+      stdout.write(String.fromCharCode("A".codeUnitAt(0) +i-1));
+      stdout.write(" ");
+      j++;
+    }
+    print("");
+    i++;
+  }
+}
